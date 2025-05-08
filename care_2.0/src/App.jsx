2c423e44db5d2aa09b8ed20f71602e0ec18e0bdb
Kirby-Rose <kirbyrosemarks@gmail.com>
2025-05-08 14:33:47 +0930

    Branding Bar upload

--- care_2.0/src/App.jsx
+++ care_2.0/src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { Container } from "@mui/material";
 import AppRoutes from "./AppRoutes";
 import HeaderNavBar from "./components/HeaderNavbar";
+import BrandingBar from "./components/BrandingBar";
 
 function App() {
   return (
     <>
+      <BrandingBar />
       <HeaderNavBar />
       <Container
         sx={{
